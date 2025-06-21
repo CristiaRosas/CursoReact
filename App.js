b831e3900a523ea35add3a0dc3653727b1c71d4f
User <crosas-2023500@kinal.edu.gt>
2025-06-20 22:15:29 -0600

    in screen

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import MainStackNavigator from './navigation/MainNavigation';
+
+export default function App() {
+  return (
+    <MainStackNavigator/>
+  );
+}
+
